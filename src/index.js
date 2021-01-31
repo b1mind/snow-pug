d9454a3d4c165f902b4128121038828cbf2031f7
b1mind <brent@1mindsolutions.com>
2021-01-31 15:13:55 -0700

    😩 base rework 🕳 review data objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log('working?')
+
+const nav = document.querySelector('.navMain')
+const canHazBurger = document.querySelector('.canHazBurger')
+
+canHazBurger.addEventListener('click', e => {
+  nav.classList.toggle('hidden')
+})
