ddc917a19a700d030d683d8f5d7ef6b8126a3eaa
b1m1nd <brent@1mindsolutions.com>
2021-01-31 15:14:16 -0700

    Updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log("working?");
+console.log("working?");var e=document.querySelector(".navMain"),n=document.querySelector(".canHazBurger");n.addEventListener("click",c=>{e.classList.toggle("hidden")});
