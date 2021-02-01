d8ab6f71133f4d0f0d590ba12085011a794066ce
b1mind <brent@1mindsolutions.com>
2021-01-31 20:21:55 -0700

    📦 updgaded

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,44 +1,9 @@
-/** @type {import("snowpack").SnowpackUserConfig } */
-
-const rollupPlugin = [
-  'snowpack-plugin-rollup-bundle',
-  {
-    //fixme rollup working with preprocessor
-    emitHtmlFiles: false,
-
-    // equivalent to inputOptions.input from Rollup
-    entrypoints: './src/index.js',
-
-    extendConfig: config => {
-      // https://rollupjs.org/guide/en/#outputoptions-object
-      config.outputOptions = {
-        file: './build/dist/index.js',
-        format: 'cjs',
-        plugins: [
-          // need preprocessor ?
-        ],
-      }
-
-      // https://rollupjs.org/guide/en/#inputoptions-object
-      config.inputOptions = {
-        input: './src/index.js',
-      }
-
-      return config
-    },
-  },
-]
-
 module.exports = {
-  //start config obj
-
-  //currently the only way I can get build to make a min/clean dist.
-  experiments: {
-    optimize: {
-      bundle: true,
-      minify: true,
-      target: 'es2018',
-    },
+  // build options? using es build
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2018',
   },
 
   mount: {
@@ -48,7 +13,6 @@ module.exports = {
   },
 
   plugins: [
-    // rollupPlugin,
     // '@snowpack/plugin-svelte',
     ['@snowpack/plugin-sass', {}],
     ['@marlonmarcello/snowpack-plugin-pug', {}],
@@ -62,21 +26,5 @@ module.exports = {
 
   buildOptions: {
     baseUrl: './',
-  },
-
-  install: [
-    /* ... */
-  ],
-
-  installOptions: {
-    /* ... */
-  },
-
-  proxy: {
-    /* ... */
-  },
-
-  alias: {
-    /* ... */
   },
 }
